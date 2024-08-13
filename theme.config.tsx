83418fb7454e3d52453326b356ed95dbd141a5f2
A-fauzi <akhmadfauzi0101@gmail.com>
2024-08-13 21:01:02 +0700

    update theme config

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: (
@@ -21,6 +23,55 @@ const config: DocsThemeConfig = {
     </span>
   </>
 ),
+  head: function useHead() {
+    const { title } = useConfig()
+    const { route } = useRouter()
+    const socialCard =
+      route === '/' || !title
+        ? 'https://nextra.site/og.jpeg'
+        : `https://nextra.site/api/og?title=${title}`
+
+    return (
+      <>
+        <meta name="msapplication-TileColor" content="#fff" />
+        <meta name="theme-color" content="#fff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta
+          name="description"
+          content="Make beautiful websites with Next.js & MDX."
+        />
+        <meta
+          name="og:description"
+          content="Make beautiful websites with Next.js & MDX."
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={socialCard} />
+        <meta name="twitter:site:domain" content="nextra.site" />
+        <meta name="twitter:url" content="https://nextra.site" />
+        <meta
+          name="og:title"
+          content={title ? title + ' – Nextra' : 'Nextra'}
+        />
+        <meta name="og:image" content={socialCard} />
+        <meta name="apple-mobile-web-app-title" content="Nextra" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link
+          rel="icon"
+          href="/favicon-dark.svg"
+          type="image/svg+xml"
+          media="(prefers-color-scheme: dark)"
+        />
+        <link
+          rel="icon"
+          href="/favicon-dark.png"
+          type="image/png"
+          media="(prefers-color-scheme: dark)"
+        />
+      </>
+    )
+  },
   project: {
     link: 'https://github.com/Anima-Unity',
   },
