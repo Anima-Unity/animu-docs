e5f77c211d5289beb08d554179146794019dc238
A-fauzi <akhmadfauzi0101@gmail.com>
2024-08-13 11:37:32 +0700

    update documents

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Anima Unity Docs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/Anima-Unity',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.com/invite/BmcsMbArvR',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Anima Unity Docs',
   },
 }
 
